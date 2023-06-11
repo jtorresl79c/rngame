b7d52bc2afdf0e7854ae62321992796c5543667d
Jason Torres Luis <jtorresl79c@gmail.com>
2023-06-11 00:49:55 -0700

    52. Adding Visual Feedback to the Buttons - En iOS el ripple efect agregado anteriormente no funciona, es por eso que creamos una forma que si funcione para iOS, esta forma consiste que al presionar el boton cambiemos la opacidad del boton, de esta forma funciona tanto iOS como para android, lo malo es que parece ser que el riple effect deja de funcionar y solo funciona el nuevo feedback de opacidad que se agrego

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -15,7 +15,14 @@ function PrimaryButton(props) //function PrimaryButton({children}) destructaring
                 // buttonInnerContainer en su propiedad backgroundColor controla el color, si se quita esta propiedad
                 // android_ripple el boton se vuelve solido y el color de fondo ahora se deberia de controlar desde
                 // buttonOuterContainer #ZXXS1
-                style={styles.buttonInnerContainer}
+
+                // el prop "style" puede recibir o un elemento de StyleSheet.create O una funcion, esta funcion retorna un objeto 
+                // llamado pressData, y dentro de ese objeto hay una propiedad llamada pressed (pressData.pressed), pressed es true
+                // si el elemento ha sido presionado, si ha sido presionado retornamos un array el cual aplica los estilos mencionados
+                // en el array, de lo contrario si es false retornamos el estilo normal buttonInnerContainer.
+                style={ ({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer }
+                // [styles.buttonInnerContainer, styles.pressed] si tenemos varios estilos que queremos aplicar podemos mandar un 
+                // array y de esta forma todos los estilos mencionados en el array se aplicaran.
             >
                 <Text style={styles.buttonText}>{props.children}</Text>
             </Pressable>
@@ -29,7 +36,7 @@ const styles = StyleSheet.create({
         margin: 4,
         overflow: 'hidden' // al parecer el android_ripple agrega estilos extras, haciendo que el borderRadius no se vea porque
         // se ve mas ancho de lo normal el Pressable, para que esto no afecte el borderRadius ponemos el overflow hidden de lo
-        // contrario el Pressable sera muy ancho y abra un overflow haciendo que el borderRadius no se vea. #ZXXS1
+        // contrario el Pressable sera muy ancho y abra un overflow haciendo que el borderRadius no se vea. #ZXXS1d
 
     },
     buttonInnerContainer: {
@@ -41,6 +48,9 @@ const styles = StyleSheet.create({
     buttonText: {
         color: 'white',
         textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
     }
 })
 
