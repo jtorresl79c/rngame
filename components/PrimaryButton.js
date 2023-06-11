be8a4eff938974a92817674aaa20e75dd5809a93
Jason Torres Luis <jtorresl79c@gmail.com>
2023-06-11 00:32:46 -0700

    52. Adding Visual Feedback to the Buttons - Se agrego el efecto ripple pero solo para android, esta es la forma en la que se crea un boton perzonalido, un <Pressable> dentro de un <View>, es la forma en la que funciona y este orden asi se debe de poner siempre, por lo que no hay que romperse la cabeza del porque se acomoda de esta forma

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -3,26 +3,39 @@ import { View, Text, Pressable, StyleSheet } from "react-native"
 function PrimaryButton(props) //function PrimaryButton({children}) destructaring sixtax
 {
 
-    function pressHandler(){
+    function pressHandler() {
         console.log('Presed!!!')
     }
 
     return (
-        <Pressable onPress={pressHandler}>
-            <View style={styles.buttonContainer}>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable 
+                onPress={pressHandler} 
+                android_ripple={{ color: '#640233' }} // al parecer esto vuelve invisible el contenedor, ya que el 
+                // buttonInnerContainer en su propiedad backgroundColor controla el color, si se quita esta propiedad
+                // android_ripple el boton se vuelve solido y el color de fondo ahora se deberia de controlar desde
+                // buttonOuterContainer #ZXXS1
+                style={styles.buttonInnerContainer}
+            >
                 <Text style={styles.buttonText}>{props.children}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        backgroundColor: '#72063c',
+    buttonOuterContainer: {
         borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden' // al parecer el android_ripple agrega estilos extras, haciendo que el borderRadius no se vea porque
+        // se ve mas ancho de lo normal el Pressable, para que esto no afecte el borderRadius ponemos el overflow hidden de lo
+        // contrario el Pressable sera muy ancho y abra un overflow haciendo que el borderRadius no se vea. #ZXXS1
+
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
         paddingVertical: 8,
         paddingHorizontal: 16,
-        margin: 4,
         elevation: 2
     },
     buttonText: {
