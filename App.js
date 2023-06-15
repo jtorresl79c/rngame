d7e623a5feb2077e26913860ca7cd17a86cf705f
Jason Torres Luis <jtorresl79c@gmail.com>
2023-06-15 16:03:53 -0700

    54. Coloring the Components & The Overall App - Se agrego un color de fondo amarillo en App.js

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <View style={styles.rootScreen}>
+      <StartGameScreen/>
+    </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50, // LA UNIDAD DE TODOS ESTOS SON PIXELES
-    backgroundColor: '#be983f',
-    alignItems: 'center',
-    padding: 40
+  rootScreen: {
+    backgroundColor: '#ddb52f',
+    flex: 1
+    // flex: 1,
+    // paddingTop: 50, // LA UNIDAD DE TODOS ESTOS SON PIXELES
+    // alignItems: 'center',
+    // padding: 40
     // justifyContent: 'center'
   }
 });
