ce847160105ea0a085bfab16beda3373c89a428b
Jason Torres Luis <jtorresl79c@gmail.com>
2023-06-19 14:45:23 -0700

    60. Starting Work on the Game Screen - Aqui solo se puso un poco de maquetacion en GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { View,Text,StyleSheet } from "react-native";
 
 function GameScreen(){
-    return <Text>GameScreen</Text>
+    return (
+        <View>
+            <Text>Opponent's Guess</Text>
+            {/* GUESS */}
+            <View>
+                <Text>Higuer or lower?</Text>
+            </View>
+            <View>
+                {/* LOG ROUNDS */}
+            </View>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    flex: 1,
+    padding: 12
+})
 
 export default GameScreen
