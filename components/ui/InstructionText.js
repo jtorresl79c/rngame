309764047e2b615b62b39b7639b208d14ba64cb7
Jason Torres Luis <jtorresl79c@gmail.com>
2023-07-02 01:24:01 -0700

    68. Using 'Cascading Styles'

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -2,8 +2,11 @@ import { StyleSheet, Text } from "react-native"
 
 import Colors from "../../constants/colors"
 
-function InstructionText({children}){
-    return <Text style={styles.instructionText}>{children}</Text>
+function InstructionText({children, style}){ // style es un nombre opcional, no a fuerzas debe de ser ese nombre
+    return <Text style={[styles.instructionText, style]}>{children}</Text>
+    // 'style' puede recibir un objeto de StyleSheet.create O un arreglo de objetos de StyleSheet.create,
+    // el segundo reemplaza al primero y el primero es reemplazado por el segundo (que al final viene siendo
+    // lo mismo), de esta forma podemos enviar un prop como style.
 }
 
 const styles = StyleSheet.create({
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -59,10 +59,17 @@ function GameScreen(props) {
             <Title>Opponent's Guess</Title>
                 <NumberContainer>{currentGuess}</NumberContainer>
             <Card>
-                <InstructionText>Higher or lower?</InstructionText>
-                <View>
-                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
-                    <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                <InstructionText style={styles.instructionText}>Higher or lower?</InstructionText>
+                <View style={styles.buttonsContainer}>
+
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                    </View>
+
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                    </View>
+
                 </View>
             </Card>
             <View>
@@ -76,6 +83,15 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 24
+    },
+    instructionText: {
+        marginBottom: 12
+    },
+    buttonsContainer: {
+        flexDirection: 'row'
+    },
+    buttonContainer: {
+        flex: 1
     }
 })
 
