aed00e0f4c0c9c7ae7cc14fab2831fd3d292298a
Jason Torres Luis <jtorresl79c@gmail.com>
2023-07-06 16:03:58 -0700

    82. Adjusting Image Sizes with the Dimensions API

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, StyleSheet } from "react-native"
+import { Text, View, Image, StyleSheet, Dimensions } from "react-native"
 import Title from '../components/ui/Title'
 import PrimaryButton from '../components/ui/PrimaryButton'
 import Colors from '../constants/colors'
@@ -19,6 +19,10 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}){
     </View>
 }
 
+
+const deviceWidth = Dimensions.get('window').width
+
+
 const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
@@ -27,9 +31,9 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 200,
+        width: deviceWidth < 380 ? 150 : 300,
+        height: deviceWidth < 380 ? 150 : 300,
+        borderRadius: deviceWidth < 380 ? 75 : 150,
         borderWidth: 3,
         borderColor: Colors.primary800,
         overflow: 'hidden',
