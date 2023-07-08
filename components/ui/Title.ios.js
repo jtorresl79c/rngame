f439d83e86b9241f60fab14c214c1d58b2aab2c6
Jason Torres Luis <jtorresl79c@gmail.com>
2023-07-07 19:43:31 -0700

    88. Writing Platform-specific Code with the Platform API

---
+++ components/ui/Title.ios.js
@@ -0,0 +1,22 @@
+import {Text, StyleSheet, Platform} from 'react-native'
+
+function Title(props){
+    return <Text style={styles.title}>{props.children}</Text>
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 24,
+        color: 'white',
+        textAlign: 'center',
+        // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        // borderWidth: Platform.select({ios: 0, android: 2}),
+        borderWidth: 0,
+        borderColor: 'white',
+        padding: 12,
+        maxWidth: '80%'
+    }
+})
+
+export default Title
